638d54988c32f2cec8188378c9f49fe1a8c353a8
bishnuf66 <bishowkarmabishnu2024@gmail.com>
2025-04-10 06:07:26 +0545

    updating porjects

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -30,7 +30,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           <img
             src={image}
             alt={name}
-            className="w-full h-full object-cover transform transition-transform duration-300 group-hover:scale-110"
+            className="w-full h-full object-contain transform transition-transform duration-300 group-hover:scale-110"
           />
           <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
         </div>
