21d00d0d5da731ecfac6704c27538a95949d3d66
bishnuf66 <bishowkarmabishnu2024@gmail.com>
2025-12-08 14:12:19 +0545

    latest code

--- fix-rls.sql
+++
@@ -1,4 +0,0 @@
--- Fix RLS policy to allow public insert
-DROP POLICY IF EXISTS "Enable insert for authenticated users" ON projects;
-CREATE POLICY "Enable insert for all users" ON projects
-  FOR INSERT WITH CHECK (true);
